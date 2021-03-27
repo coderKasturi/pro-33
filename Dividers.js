91d42db3ed4ebfe3ef649af38704dfc9f1ce1d6d
coderKasturi <73377018+coderKasturi@users.noreply.github.com>
2021-03-27 23:58:59 +0530

    Add files via upload

---
+++ Dividers.js
@@ -0,0 +1,20 @@
+class Dividers {
+    constructor(x, y, w, h) {
+        var options = {
+
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.x;
+        this.y;
+        this.w = 10;
+        this.h = 50;
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x, pos.y, this.w, this.h);
+    }
+};
